5eabb42fc314558483e2d30b0ce568eb229188ee
guido <guidorolando91@gmail.com>
2018-05-01 13:00:30 -0400

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: { title: 'Contact List'}
+  },
+  {
+    path: '',
+    redirectTo: '/contact',
+    pathMatch: 'full',
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true}
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
