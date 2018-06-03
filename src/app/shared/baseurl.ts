41168f23802ec73606e30793abd16b27ba3ef467
guido <guidorolando91@gmail.com>
2018-06-03 16:46:22 -0400

    deploy

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,3 +1,4 @@
+// export const baseURL = 'https://ssi-ssi.1d35.starter-us-east-1.openshiftapps.com';
 export const baseURL = 'http://localhost:8080/';
 
 // export const base_urlRest = 'swagger-ui.html#/';
