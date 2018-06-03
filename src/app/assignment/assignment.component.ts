213037156cc756744fd982e64a4300892dd1aace
lee-ivanov <lee.19.ra@gmail.com>
2018-06-03 11:28:58 -0400

    eq-in-asign

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -11,9 +11,12 @@ import { MaterialElementService } from '../security/services/material-element.se
 export class AssignmentComponent implements OnInit {
 
   public employees;
-  constructor() { }
+  public equipments;
+  constructor(private materialEq:MaterialElementService) { }
 
   ngOnInit() {
+
+    this.materialEq.getMaterial().subscribe(data => { this.equipments = data});
     //this.employeeAssign.getEmployees().subscribe(data => { this.employees = data});
   }
 
