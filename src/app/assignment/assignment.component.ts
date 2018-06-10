ad9dcdf66484e142800b85ae9e4158f5192f8ce8
lee-ivanov <lee.19.ra@gmail.com>
2018-06-09 22:41:45 -0400

    assignment attempt :(

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -3,6 +3,8 @@ import {Router, ActivatedRoute} from '@angular/router';
 import { AssignmentReportService } from '../security/services/assignment-report.service';
 import { MaterialElementService } from '../security/services/material-element.service';
 import { EmployeeService } from '../services/employee.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AssignmentService } from '../security/services/assignment.service';
 
 @Component({
   selector: 'app-assignment',
@@ -13,9 +15,13 @@ export class AssignmentComponent implements OnInit {
 
   materials:[{}];
   employees:[{}];
+  assignmentForm: FormGroup;
 
-  constructor(private  equipment: MaterialElementService,
-              private  employee: EmployeeService) {
+  constructor(private equipment: MaterialElementService,
+              private employee: EmployeeService,
+              private assignmentService: AssignmentService,
+              private fa:FormBuilder) {
+                this.createFormAssign();
   }
 
   ngOnInit() {
@@ -23,4 +29,13 @@ export class AssignmentComponent implements OnInit {
     this.employee.getEmployees().subscribe(data => { this.employees = data});
   }
 
+  private createFormAssign(){
+    this.assignmentForm =this.fa.group({
+      firstname:['',Validators.required],
+      name:['',Validators.required],
+      quantity:['',Validators.required]
+    });
+  }
+  ngOnSubmit(){
+  }
 }
--- src/app/security/services/assignment.service.ts
+++ src/app/security/services/assignment.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+import { baseURL } from '../../shared/baseurl';
 
 @Injectable()
 export class AssignmentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+saveAssignment(data: any): Observable<any>{
+    const params = JSON.stringify(data);
+    return this.http.post(baseURL+'/assignment', {Headers});
+  }
 }
