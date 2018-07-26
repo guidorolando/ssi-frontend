845b8df2bc707368f21fc5847d900e8573fd20d7
guido <guidorolando91@gmail.com>
2018-07-26 03:56:52 -0400

    change server

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,4 +1,5 @@
-export const baseURL = 'https://ssi-ssi.1d35.starter-us-east-1.openshiftapps.com/';
+export const baseURL = 'https://ssis-ssi-backend.193b.starter-ca-central-1.openshiftapps.com/';
+
 // export const baseURL = 'http://localhost:8080/';
 
 // export const base_urlRest = 'swagger-ui.html#/';
